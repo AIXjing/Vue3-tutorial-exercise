b9adc4e80059d2effe36aac3574904387c47fabd
Jing Ai <ai.jing.cph@gmail.com>
2020-10-18 17:21:45 +0200

    Create a calendar

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,15 +2,17 @@
   <AppHeader />
   <div class="w-full flex">
 <!--    <Heros />-->
+    <Calendar />
   </div>
 </template>
 
 <script>
 import AppHeader from "@/components/AppHeader";
+import Calendar from "@/components/Calendar";
 // import Heros from "@/components/Heros";
 
 
 export default {
-  components:{AppHeader}
+  components:{AppHeader, Calendar}
 }
 </script>
