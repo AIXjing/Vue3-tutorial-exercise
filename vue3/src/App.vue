4c1c54ee64c9e463d243b259260581f4b336a3ad
Jing Ai <ai.jing.cph@gmail.com>
2020-10-18 15:13:41 +0200

    add components

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,76 +1,16 @@
 <template>
-  <div>
-    <h1 class="text-center"> My Heros </h1>
-    <ul>
-      <li v-for="(hero,index) in heros" :key="hero.name">
-        <div>{{ hero.name }}
-          <button @click="remove(index)"> x</button>
-        </div>
-      </li>
-    </ul>
-
-    <form @submit.prevent="addHero()">
-      <input v-model="newhero" placeholder="Type new Hero!"/>
-      <button type="submit">Add Hero</button>
-    </form>
+  <AppHeader />
+  <div class="w-full flex">
+<!--    <Heros />-->
   </div>
 </template>
 
-
 <script>
+import AppHeader from "@/components/AppHeader";
+// import Heros from "@/components/Heros";
+
+
 export default {
-  data() {
-    return {
-      heros: [
-        {name: "1"},
-        {name: "2"},
-        {name: "3"},
-      ],
-      newhero: "",
-      // fname: "Sar",
-      // lname: "Shri",
-      attribute: "value",
-      isDisable: false,
-    }
-  },
-
-  methods: {
-    addHero() {
-      if (this.newhero == "") {
-        alert("Type hero name!");
-        return
-      }
-      this.heros.push({name: this.newhero});
-      this.newhero = '';
-    },
-    remove(index) {
-      this.heros = this.heros.filter((hero, i) => i !== index)
-    }
-    // randM() {
-    //   return Math.random();
-    // },
-    // setFullName(){
-    //   this.fullname = 'Bitfumes Tutorial'
-    // }
-  },
-
-  computed: {
-    herosCount() {
-      return this.heros.length
-    },
-    // randC() {
-    //   return Math.random()
-    // },
-    // fullname: {
-    //   get() {
-    //     return `The fullname is ${this.fname} ${this.lname}`
-    //   },
-    //   set(fullname) {
-    //     const values = fullname.split(" ");
-    //     this.fname = values[0];
-    //     this.lname = values[1];
-    //   },
-    // },
-  }
+  components:{AppHeader}
 }
 </script>
---
+++ vue3/src/components/Heros.vue
@@ -0,0 +1,82 @@
+<template>
+
+    <div class="m-auto">
+      <h1 class="text-2xl text-center"> My Heros </h1>
+      <ul>
+        <li class="flex justify-between" v-for="(hero,index) in heros" :key="hero.name">
+          <div>
+            {{ hero.name }}
+          </div>
+          <button @click="remove(index)"> x</button>
+        </li>
+      </ul>
+
+      <form class="mt-10" @submit.prevent="addHero()">
+        <input
+            class="border rounded-lg"
+            v-model="newhero"
+            placeholder="Type new Hero!"
+        />
+        <button class="border rounded" type="submit">Add Hero</button>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      heros: [
+        {name: "Fuyang Baby"},
+        {name: "Wonder Jing"},
+        {name: "Fector"},
+        {name: "Fugo"}
+      ],
+      newhero: "",
+      // fname: "Sar",
+      // lname: "Shri",
+      attribute: "value",
+      isDisable: false,
+    }
+  },
+  methods: {
+    addHero() {
+      if (this.newhero == "") {
+        alert("Type hero name!");
+        return
+      }
+      this.heros.push({name: this.newhero});
+      this.newhero = '';
+    },
+    remove(index) {
+      this.heros = this.heros.filter((hero, i) => i !== index)
+    }
+    // randM() {
+    //   return Math.random();
+    // },
+    // setFullName(){
+    //   this.fullname = 'Bitfumes Tutorial'
+    // }
+  },
+
+  computed: {
+    herosCount() {
+      return this.heros.length
+    },
+    // randC() {
+    //   return Math.random()
+    // },
+    // fullname: {
+    //   get() {
+    //     return `The fullname is ${this.fname} ${this.lname}`
+    //   },
+    //   set(fullname) {
+    //     const values = fullname.split(" ");
+    //     this.fname = values[0];
+    //     this.lname = values[1];
+    //   },
+    // },
+  }
+}
+
+</script>
